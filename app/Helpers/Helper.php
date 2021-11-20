bcca59865d1d6276fb37bdecb06a26ed82f7eb0d
Yoni Widhi Cahyadi <yoni@mit.id>
2021-11-20 18:14:13 +0700

    Update: Helper and Add Transaction temporary Request class

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -6,8 +6,10 @@ use \App\Models\User;
 
 class Helper {
     
-    public static function balance ($perDate = null, $startDate = null, $categoryId = null)
-    {
+    /**
+     * Count the balance based on date or category
+     */
+    public static function balance ($perDate = null, $startDate = null, $categoryId = null) {
         $transactionQuery = DB::table('transactions');
 
         if ($perDate) {
@@ -24,13 +26,37 @@ class Helper {
 
         return $transactions->sum(function ($transaction) {
 
-            $balance = $transaction->transaction_status_id ? $transaction->amount : -$transaction->amount;
+            $balance = ($transaction->transaction_status_id == 1) ? $transaction->amount : -$transaction->amount;
 
-            User::findOrFail(auth()->user()->id)->update([
-                'balance' => $balance
-            ]);
+            // User::findOrFail(auth()->user()->id)->update([
+            //     'balance' => $balance
+            // ]);
 
             return $balance;
         });
+    }
+
+    /**
+     * Get Months
+     */
+    public static function getMonths () {
+
+        return [
+            'January', 'February', 'March', 'April', 'May', 'Juni', 'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+    }
+
+    /**
+     * Get Years from 2019
+     */
+    public static function getYears() {
+
+        $yearRange = range(2019, date('Y'));
+
+        foreach ($yearRange as $year) {
+            $years[$year] = $year;
+        }
+
+        return $years;
     }
 }
